09ffa7355b69ca4c60b96c722fa7753dc2f85ec4
andrewj12763 <andrewj12763@gmail.com>
2020-01-31 18:36:58 -0700

    seperating functions

--- elixir_sdet_exercise/test/elixir_sdet_exercise_test.exs
+++ elixir_sdet_exercise/test/elixir_sdet_exercise_test.exs
@@ -6,26 +6,44 @@ defmodule ElixirSdetExerciseTest do
   # Start hound session and destroy when tests are run
   hound_session()
 
-  test "goes to google" do
-    navigate_to "https://en-gb.facebook.com/r.php?locale=en_GB&display=page"
-    # Set elements to variables fro re-use
+  test "Sign-up No input" do
+  navigate_to "https://en-gb.facebook.com/r.php?locale=en_GB&display=page"
+    # Set elements to Variables
     signUpBTN = find_element(:id, "u_0_14")
+    # Sign up without inputting info
+    click(signUpBTN)
+    take_screenshot("noInput.png")
+   
+  end
+  test "Sign-up No Password" do
+  navigate_to "https://en-gb.facebook.com/r.php?locale=en_GB&display=page"
+    # Set elements to Variables
+    signUpBTN = find_element(:id, "u_0_14")
+    fName = find_element(:id, "u_0_n")
+    lName = find_element(:id, "u_0_p")
+    email = find_element(:id, "u_0_s")
+    rEmail = find_element(:id, "u_0_v")
+    gender = find_element(:id, "u_0_6")
+     # Sign up with no password
+    fill_field(fName, "John")
+    fill_field(lName, "Smith")
+    fill_field(email, "johnsmith@gmail.com")
+    fill_field(rEmail, "johnsmith@gmail.com")
+    click(gender)
+    click(signUpBTN)
+    take_screenshot("No Password.png")
+   
+  end
+  test "Sign-up Invalid Email" do
+  navigate_to "https://en-gb.facebook.com/r.php?locale=en_GB&display=page"
+    # Set elements to Variables
+   signUpBTN = find_element(:id, "u_0_14")
     fName = find_element(:id, "u_0_n")
     lName = find_element(:id, "u_0_p")
     email = find_element(:id, "u_0_s")
     password = find_element(:id, "u_0_x")
     gender = find_element(:id, "u_0_6")
 
-    # Sign up without inputting info
-    click(signUpBTN)
-    take_screenshot("noInput.png")
-     # Sign up with no password
-    fill_field(fName, "John")
-    fill_field(lName, "Smith")
-    fill_field(email, "johnsmith.com")
-    click(gender)
-    click(signUpBTN)
-    take_screenshot("No Password.png")
     # Sign up with invalid email
     fill_field(fName, "John")
     fill_field(lName, "Smith")
@@ -34,8 +52,24 @@ defmodule ElixirSdetExerciseTest do
     click(gender)
     click(signUpBTN)
     take_screenshot("invalid email.png")
+   
+  end
+  test "Sign-up Invalid Phone" do
+  navigate_to "https://en-gb.facebook.com/r.php?locale=en_GB&display=page"
+   # Set elements to Variables
+    signUpBTN = find_element(:id, "u_0_14")
+    fName = find_element(:id, "u_0_n")
+    lName = find_element(:id, "u_0_p")
+    email = find_element(:id, "u_0_s")
+    password = find_element(:id, "u_0_x")
+    gender = find_element(:id, "u_0_6")
+
      # Sign up with invalid phone
+     fill_field(fName, "John")
+     fill_field(lName, "Smith")
      fill_field(email, "801801801")
+     click(gender)
+     click(signUpBTN)
      take_screenshot("invalid Phone number.png")
    
   end
